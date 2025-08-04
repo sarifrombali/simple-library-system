56a624511aa47d4be1ec050c1bb0c8c59b725289
sarifrombali <134642092+sarifrombali@users.noreply.github.com>
2025-08-04 16:07:02 +0800

    created menu using switch case

--- simplelibrarysystem/src/main/java/com/mycompany/simplelibrarysystem/Simplelibrarysystem.java
+++ simplelibrarysystem/src/main/java/com/mycompany/simplelibrarysystem/Simplelibrarysystem.java
@@ -1,28 +1,56 @@
 package com.mycompany.simplelibrarysystem;
 
+import java.util.Scanner;
 
 public class Simplelibrarysystem {
-    
-//    class Book {
-//        public String title;
-//        public String author;
-//        public String publicationDate;
-//        public int copiesAvailable;
-//    }
 
     public static void main(String[] args) {
-        double income = 30000, tax;
-        if(income <= 15000) {
-            tax = 0;
-        }
-        else if (income <= 25000) {
-            tax = 0.05 * (income - 15000);
-        }
-        else {
-            tax = 0.05 * (income - (25000 - 15000));
-            tax += 0.10 * (income - 25000);
+        
+        Scanner sc = new Scanner(System.in);
+        
+        boolean isAvailable = false;
+        String requestType;
+        String isLate;
+        int lateFee = 5;
+        boolean processDone = false;
+        
+        System.out.print("Would you like to BORROW or RETURN a book? : ");
+        requestType = sc.nextLine();
+        
+        while(!processDone) {
+            switch(requestType) {
+                case "BORROW":
+                    if(isAvailable) {
+                        System.out.println("Done! Remember to return it in 14 days!");
+                    } else {
+                        System.out.println("Sorry, currently it is not available.");
+                    };
+                    processDone = true;
+                    break;
+
+                case "RETURN":
+                    System.out.println("Are you returning this book late? (Y/N): ");
+                    isLate = sc.nextLine();
+                    switch(isLate) {
+                        case "Y":
+                            System.out.println("You are late! A $" + lateFee + " fee is due.");
+                            processDone = true;
+                            break;
+                        case "N":
+                            System.out.println("Thank you for returning the book on time!");
+                            processDone = true;
+                            break;
+                        default:
+                            System.out.println("Error! Please type in Y for 'yes' or N for 'no'");
+                    }
+                    break;
+
+                default:
+                    System.out.println("Error! Please type in BORROW or RETURN.");
+                    requestType = sc.nextLine();
+            }
         }
         
-        System.out.println("This year's tax: $" + tax);
     }
+    
 }

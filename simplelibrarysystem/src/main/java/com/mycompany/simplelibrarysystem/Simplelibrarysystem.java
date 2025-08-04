8da100b96b167c602c52bebade22042b8436584c
sarifrombali <134642092+sarifrombali@users.noreply.github.com>
2025-08-04 14:03:59 +0800

    learning else if

--- simplelibrarysystem/src/main/java/com/mycompany/simplelibrarysystem/Simplelibrarysystem.java
+++ simplelibrarysystem/src/main/java/com/mycompany/simplelibrarysystem/Simplelibrarysystem.java
@@ -11,6 +11,18 @@ public class Simplelibrarysystem {
 //    }
 
     public static void main(String[] args) {
+        double income = 57640, tax;
+        if(income <= 15000) {
+            tax = 0;
+        }
+        else if (income <=25000) {
+            tax = 0.05 * (income - 15000);
+        }
+        else {
+            tax = 0.05 * (income - (25000 - 15000));
+            tax += 0.10 * (income - 25000);
+        }
         
+        System.out.println("This year's tax: $" + tax);
     }
 }

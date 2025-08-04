fce0b7b1801711762b788f5007f82f1601961971
sarifrombali <134642092+sarifrombali@users.noreply.github.com>
2025-08-04 14:02:31 +0800

    init

---
+++ simplelibrarysystem/src/main/java/com/mycompany/simplelibrarysystem/Simplelibrarysystem.java
@@ -0,0 +1,16 @@
+package com.mycompany.simplelibrarysystem;
+
+
+public class Simplelibrarysystem {
+    
+//    class Book {
+//        public String title;
+//        public String author;
+//        public String publicationDate;
+//        public int copiesAvailable;
+//    }
+
+    public static void main(String[] args) {
+        
+    }
+}
